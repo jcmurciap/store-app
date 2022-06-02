4483f77a840708967e3161bcadfe6f0e6e710d7e
enigma <camilo.murcia.pimentel@gmail.com>
2022-06-01 23:40:55 -0500

    test form is mounted

---
+++ src/form/form.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {screen, render} from '@testing-library/react';
+import {Form} from './form';
+
+describe('When the form is mounted', () => {
+    it('There must be a create product form page', ()  => {
+        
+        render(<Form />);
+        expect(screen.getByRole('heading',/create product/i)).toBeInTheDocument();
+    })
+
+})
