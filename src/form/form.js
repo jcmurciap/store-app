54d1ad70d8aa9a9e169fb8fc1495492f61af2d4a
enigma <camilo.murcia.pimentel@gmail.com>
2022-06-06 21:01:18 -0500

    Add testing examples

--- src/form/form.js
+++ src/form/form.js
@@ -1,5 +1,29 @@
 import React from "react";
+import TextField from '@mui/material/TextField';
+import { InputLabel, Select, MenuItem } from "@mui/material";
+import Button from "@mui/material/Button";
 
-export const Form = () => <h1>Create product</h1>
+export const Form = () => (
+    <>
+        <h1>Create product</h1>
+        <form>
+            <TextField id="name" label="name" variant="filled" />
+            <TextField id="size" label="size" variant="filled" />
+            <InputLabel id="type">Type</InputLabel>
+            <Select
+                labelId="type"
+                id="demo-simple-select"
+                value="a"
+                label="type"
+            >
+                <MenuItem value="electronic">Electronic</MenuItem>
+                <MenuItem value="furniture">Furniture</MenuItem>
+                <MenuItem value="clothing">Clothing</MenuItem>
+            </Select>
+            <Button variant="outlined">Submit</Button>
+
+        </form>
+    </>
+)
 
 export default Form;
--- src/form/form.test.js
+++ src/form/form.test.js
@@ -1,12 +1,41 @@
 import React from 'react';
-import {screen, render} from '@testing-library/react';
+import {screen, render, fireEvent} from '@testing-library/react';
 import {Form} from './form';
 
 describe('When the form is mounted', () => {
+    
+    beforeEach(() => render(<Form />));
+    
     it('There must be a create product form page', ()  => {
         
-        render(<Form />);
         expect(screen.getByRole('heading',/create product/i)).toBeInTheDocument();
-    })
+    });
 
-})
+    it('Should exist the fields: name, size, type (electronic,furniture, clothing)', async () => {
+        
+        expect(screen.getByLabelText(/name/i)).toBeInTheDocument();
+        expect(screen.getByLabelText(/size/i)).toBeInTheDocument();
+        expect(screen.getByLabelText(/type/i)).toBeInTheDocument();
+        
+        //expect(screen.queryByText(/electronic/i)).toBeInTheDocument(); //BUG
+        //expect(screen.queryByText(/furniture/i)).toBeInTheDocument(); //BUG
+        //expect(screen.queryByText(/clothing/i)).toBeInTheDocument(); //BUG
+    });
+
+    it('Should exist the submit button', () => {
+        expect(screen.getByRole('button', {name: /submit/i})).toBeInTheDocument();
+    });
+});
+
+describe('When the user submits the form without values', () => {
+    
+    beforeEach(() => render(<Form />));
+
+    it.only('Should display validation messages', () => {
+        
+        render(<Form />);
+
+        fireEvent.click(screen.getByRole('button', { name: /submit/i }));
+        expect(screen.queryByText(/the name is required/i)).toBeInTheDocument();
+    });
+});
